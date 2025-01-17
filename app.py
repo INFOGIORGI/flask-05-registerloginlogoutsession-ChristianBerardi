f3c39c5be84d43adcf1166424305bf2aadbdd174
ChristianBerardi <chriprs7.0@gmail.com>
2025-01-17 12:39:17 +0000

    es 17/01/2025

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
-
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = '138.41.20.102'
@@ -19,11 +19,11 @@ def registrati():
     if request.method=='GET':
         return render_template("register.html")
     else:
-        nome= request.form.get("nome")
-        cognome= request.form.get("cognome")
-        username= request.form.get("username")
-        password= request.form.get("password")
-        confirmPassword= request.form.get("confirmPassword")
+        nome= request.form.get("nome", "None")
+        cognome= request.form.get("cognome", "None")
+        username= request.form.get("username", "None")
+        password= request.form.get("password", "None")
+        confirmPassword= request.form.get("confirmPassword", "None")
 
         if nome=="None" or cognome=="None" or username=="None" or password=="None" or confirmPassword=="None":
             return render_template("register.html")
@@ -37,9 +37,29 @@ def registrati():
             return render_template("register.html")
         
         query="INSERT INTO users(username,password,nome,cognome) VALUES(%s,%s,%s,%s)"
-        cursor.execute(query,(username, password, nome, cognome))
+        cursor.execute(query,(username, generate_password_hash(password), nome, cognome))
         mysql.connection.commit()
         cursor.close()
         return redirect(url_for("homepage"))
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method=='GET':
+        render_template("login.html")
+    else:
+        username= request.form.get("username", "None")
+        password= request.form.get("password", "None")
+        query="SELECT * FROM users WHERE username=%s AND password=%s"
+        cursor=mysql.connection.cursor()
+        cursor.execute(query,(username, password))
+        if cursor.fetchone():
+            return render_template("paginaLogin.html")
+        
+
+
+
+
+
+
 
 app.run(debug=True)
